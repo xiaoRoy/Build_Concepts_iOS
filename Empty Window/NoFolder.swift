f981573c2336a24f6a7b4a087dd653f1c160cd1c
Jerry Li <jerli@xogrp.com>
2022-11-19 16:36:53 +0800

    Initial Commit

---
+++ Empty Window/NoFolder.swift
@@ -0,0 +1,8 @@
+//
+//  NoFolder.swift
+//  Empty Window
+//
+//  Created by jerli on 2022/10/14.
+//
+
+import Foundation
---
+++ Empty Window/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Empty Window
+//
+//  Created by jerli on 2022/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
