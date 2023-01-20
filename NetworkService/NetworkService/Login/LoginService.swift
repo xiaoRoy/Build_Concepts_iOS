2a48206cec51b183886d60272ca662fbc1b3735d
Jerry Li <jerli@xogrp.com>
2023-01-20 22:31:27 +0800

    Added LoginService.

---
+++ NetworkService/NetworkService/Login/LoginService.swift
@@ -0,0 +1,19 @@
+//
+//  LoginService.swift
+//  NetworkService
+//
+//  Created by jerli on 2022/12/16.
+//
+
+import Foundation
+
+public class LoginService {
+    
+    func login(email: String, password: String) -> String {
+        return "user_token"
+    }
+    
+    func signup(email: String, password: String) -> String {
+        return "user_token"
+    }
+}
