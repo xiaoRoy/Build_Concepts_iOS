75bb39a6b9937131668c133ae7dfa6a751f11a76
xiaoRoy <xiaoroyg@163.com>
2025-04-15 20:30:34 +0800

    Finished  Using Preprocessor Macros (Conditional Compilation)

--- Empty Window/Empty Window/Base/Settings.swift
+++ Empty Window/Empty Window/Base/Settings.swift
@@ -7,6 +7,54 @@
 
 import Foundation
 
+protocol SettingsProvider {
+    func value(for key: String) throws -> String
+}
+
+struct BundleSettingsProvider: SettingsProvider {
+    func value(for key: String) throws -> String {
+        return try Settings.getValue(key)
+    }
+}
+
+struct ConditionalCompilationSettingsProvider: SettingsProvider {
+
+    static let firebaseKey: String = Settings.Key.firebaseKey.rawValue
+    
+    fileprivate static func getValue(_ key: String) throws -> String{
+        let settings: [String: String]
+        #if DEBUG
+            settings = DebugSettingsHolder.settingsDictionary
+        #else
+            settings = ReleaseSettingsHolder.settingsDictionary
+        #endif
+        guard let value = settings[key] else {
+            throw SettingsError.keyNotFound(key)
+        }
+        return value
+    }
+
+    func value(for key: String) throws -> String {
+        try ConditionalCompilationSettingsProvider.getValue(key)
+    }
+
+    protocol SettingsHolder {
+        static var settingsDictionary: [String: String] { get }
+    }
+
+    struct DebugSettingsHolder: SettingsHolder {
+        static var settingsDictionary: [String: String] = [
+            firebaseKey: "degbu-firebase-key"
+        ]
+    }
+
+    struct ReleaseSettingsHolder: SettingsHolder {
+        static var settingsDictionary: [String: String] = [
+            firebaseKey: "release-firebase-key"
+        ]
+    }
+}
+
 enum SettingsError: Error {
     case keyNotFound(String)
     case typeMismatch(expected: String, key: String)
@@ -15,17 +63,18 @@ enum SettingsError: Error {
 enum Settings {
 
     enum Key: String, CaseIterable {
-        
+
         case buildConfiguration = "Build Configuration"
         case bundleIdentifier = "CFBundleIdentifier"
         case apiBaseUrl = "Api Base Url"
         case bundleDisplayName = "CFBundleDisplayName"
         case bundleName = "CFBundleName"
         case xApiKey = "X API Key"
+        case firebaseKey = "Firebase Key"
     }
 
-    private static func getValue<T>(_ key: String) throws -> T {
-        
+    fileprivate static func getValue<T>(_ key: String) throws -> T {
+
         let valueToLoad = Bundle.main.object(forInfoDictionaryKey: key)
         if valueToLoad == nil {
             throw SettingsError.keyNotFound(key)
@@ -39,51 +88,78 @@ enum Settings {
 
         return value
     }
-    
+
     static func value(for key: Settings.Key) throws -> String {
         return try getValue(key.rawValue)
     }
     
-    static var allSettings:[(String, String)] {
+    private static let providers: [SettingsProvider] = [
+         BundleSettingsProvider(),
+         ConditionalCompilationSettingsProvider()
+     ]
+
+    static var allSettings: [(String, String)] {
         get throws {
-            return try Settings.Key.allCases.map {($0.rawValue, try value(for: $0))}
+            return try Settings.Key.allCases.map({
+                (key) -> (String, String) in
+                let rawKey = key.rawValue
+                var resultValue: String? = nil
+                for provider in providers {
+                    if let value = try? provider.value(for:rawKey) {
+                        resultValue = value
+                        break
+                    }
+                    
+                }
+                guard let resultValue else  {
+                    throw SettingsError.keyNotFound(rawKey)
+                }
+                return (rawKey, resultValue)
+            })
+
         }
     }
-    
+
     static var buildConfiguraion: String {
         get throws {
             return try value(for: Settings.Key.buildConfiguration)
         }
     }
-    
+
     static var bundleIdentifier: String {
         get throws {
             return try value(for: Settings.Key.bundleIdentifier)
         }
     }
-    
+
     static var apiBaseUrl: String {
         get throws {
             return try value(for: Settings.Key.apiBaseUrl)
         }
     }
-    
+
     static var bundleDisplayName: String {
         get throws {
             return try value(for: Settings.Key.bundleDisplayName)
         }
     }
-    
+
     static var bundleName: String {
         get throws {
             return try value(for: Settings.Key.bundleName)
         }
     }
-    
+
     static var xApiKey: String {
         get throws {
             return try value(for: Settings.Key.xApiKey)
         }
     }
-    
+
+    static var firebaseKey: String  {
+        get throws {
+            return try ConditionalCompilationSettingsProvider.getValue(Settings.Key.firebaseKey.rawValue)
+        }
+    }
+
 }
